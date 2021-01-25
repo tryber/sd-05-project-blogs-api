2217be68c87c3f9d00a498505a2507d7091789cf
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-01-25 15:34:42 -0200

    Pre start

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
