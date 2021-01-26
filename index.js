7b36322da701131eb7e1c158adb0f4d5dfcbc821
César Alves <alvescesar@hotmail.com>
2021-01-26 18:13:34 -0300

    Created web token (jwt)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`I hear you, stop yelling! Damn port ${port}...`));
