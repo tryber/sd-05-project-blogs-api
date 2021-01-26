25ea50bb342a7c486b29ede9910374033f598cab
pedrohcalado <pedrocalado22@gmail.com>
2021-01-26 16:37:16 -0300

    Requisito 2 - sem try catch

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -14,28 +14,24 @@ const jwtConfig = {
 const router = express.Router();
 
 router.post('/', async (req, res, _next) => {
-  try {
-    const { email, password } = req.body;
-    if (email === '') return res.status(400).json({ message: '"email" is not allowed to be empty' });
-    if (!email) return res.status(400).json({ message: '"email" is required' });
-    if (password === '') return res.status(400).json({ message: '"password" is not allowed to be empty' });
-    if (!password) return res.status(400).json({ message: '"password" is required' });
+  const { email, password } = req.body;
+  if (email === '') return res.status(400).json({ message: '"email" is not allowed to be empty' });
+  if (!email) return res.status(400).json({ message: '"email" is required' });
+  if (password === '') return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  if (!password) return res.status(400).json({ message: '"password" is required' });
 
-    const user = await User.findOne({ where: { email } });
-    if (!user || user.password !== password) return res.status(400).json({ message: 'Campos inválidos' });
-    const { _id: id } = user;
-    const { password: _, ...userWithoutPassword } = user;
-    const payloadData = {
-      iss: 'post-api',
-      aud: 'identity',
-      sub: id,
-      userData: userWithoutPassword,
-    };
-    const token = jwt.sign(payloadData, secret, jwtConfig);
-    return res.status(200).json({ token });
-  } catch (e) {
-    return res.status(500).json({ message: 'Internal error', error: e });
-  }
+  const user = await User.findOne({ where: { email } });
+  if (!user || user.password !== password) return res.status(400).json({ message: 'Campos inválidos' });
+  const { _id: id } = user;
+  const { password: _, ...userWithoutPassword } = user;
+  const payloadData = {
+    iss: 'post-api',
+    aud: 'identity',
+    sub: id,
+    userData: userWithoutPassword,
+  };
+  const token = jwt.sign(payloadData, secret, jwtConfig);
+  return res.status(200).json({ token });
 });
 
 module.exports = router;
