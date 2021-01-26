d6f4b70326e7faa9540867b4d4208532a9614855
samueap <samueap@gmail.com>
2021-01-25 21:31:04 -0300

    dotEnv and migrate: Users and Posts

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
+// ----------------------------------------------------------------------------------
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+// ----------------------------------------------------------------------------------
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Blogueirinha, Blogueirinha, Bloguerinha... só no ${PORT}`));
---
+++ migrations/20210125232547-create-post-tables.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Posts');
+  }
+};
