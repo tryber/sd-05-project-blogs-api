88acb1db3380856dcfc5ffad6fe6b458f9af4036
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-01-26 11:12:22 -0300

    commenting so as to validate knowledge

--- migrations/20210125221445-create-posts.js
+++ migrations/20210125221445-create-posts.js
@@ -35,6 +35,10 @@ module.exports = {
         onDelete: 'CASCADE',
         references: { model: 'Users', key: 'id' }
       },
+      // references.model says what table our FK is refering to, using its Model name.
+      // references.key says what column of the above foreign table is to be used as FK.
+      // onUpdate & onDelete set what happens when updating or deleting a user,
+      // here, ‘CASCADE’ means all posts related to such user also would be updated or deleted.
       published: {
 				allowNull: false,
         type: Sequelize.DATE
