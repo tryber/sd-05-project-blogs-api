c8e4e85241f2bcd832af6780f104466bbbb3f734
Andrevlc <andrevezi@gmail.com>
2021-01-26 19:38:27 -0300

    Changed the router order in post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,32 +4,6 @@ const validateJWT = require('../auth/validateJWT');
 
 const posts = Router();
 
-posts.post('/', validateJWT, async (req, res) => {
-  try {
-    const { id } = req.payload;
-    const { title, content } = req.body;
-    const createPost = await service.create(title, content, id);
-    if (createPost.error) {
-      return res.status(createPost.code).json({ message: createPost.message });
-    }
-    const createdPost = {
-      title: createPost.dataValues.title,
-      content: createPost.dataValues.content,
-      userId: createPost.dataValues.userId,
-    };
-    return res.status(201).json(createdPost);
-  } catch (e) {
-    res.send(e.message);
-  }
-});
-posts.get('/', validateJWT, async (req, res) => {
-  try {
-    const getAllPosts = await service.getAll();
-    return res.status(200).json(getAllPosts);
-  } catch (e) {
-    res.send(e.message);
-  }
-});
 posts.get('/:id', validateJWT, async (req, res) => {
   try {
     const { id } = req.params;
@@ -55,6 +29,33 @@ posts.put('/:id', validateJWT, async (req, res) => {
       res.status(401).json({ message: 'Usuário não autorizado' });
     }
     return res.status(200).json(updatePost);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+posts.post('/', validateJWT, async (req, res) => {
+  try {
+    const { id } = req.payload;
+    const { title, content } = req.body;
+    const createPost = await service.create(title, content, id);
+    if (createPost.error) {
+      return res.status(createPost.code).json({ message: createPost.message });
+    }
+    const createdPost = {
+      title: createPost.dataValues.title,
+      content: createPost.dataValues.content,
+      userId: createPost.dataValues.userId,
+    };
+    return res.status(201).json(createdPost);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+posts.get('/', validateJWT, async (req, res) => {
+  try {
+    const getAllPosts = await service.getAll();
+    return res.status(200).json(getAllPosts);
   } catch (e) {
     res.send(e.message);
   }
