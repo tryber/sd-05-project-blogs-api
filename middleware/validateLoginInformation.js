d63b66646c57dc772b05e7c962e73ac18be97bfa
rfreitasbatista <rfreitasbatista@gmail.com>
2021-01-26 17:03:37 -0300

    Requisito 2

--- middleware/validateLoginInformation.js
+++ middleware/validateLoginInformation.js
@@ -1,13 +1,24 @@
+const customReturn = (res, message, status) => res.status(status).json({ message });
+
 const validateLoginInformation = (req, res, next) => {
   const { email, password } = req.body;
   const regEx = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
-  if (!email || !password || !regEx.test(email)) {
-    return res.status(400).json({
-      message: 'Campos inválidos',
-    });
+  if (email === undefined) {
+    return customReturn(res, '"email" is required', 400);
   }
-
+  if (email.length === 0) {
+    return customReturn(res, '"email" is not allowed to be empty', 400);
+  }
+  if (!regEx.test(email)) {
+    return customReturn(res, '"email" must be a valid email', 400);
+  }
+  if (password === undefined) {
+    return customReturn(res, '"password" is required', 400);
+  }
+  if (password.length === 0) {
+    return customReturn(res, '"password" is not allowed to be empty', 400);
+  }
   next();
 };
 
