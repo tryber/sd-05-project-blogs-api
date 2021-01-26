458d5a36562c5ba5a5b29cc751c4fa94592dbedd
Christian Duessel <duessel@gmail.com>
2021-01-26 18:44:39 -0300

    lint

--- services/userServices.js
+++ services/userServices.js
@@ -1,25 +1,19 @@
 const { Users } = require('../models');
 const { createToken } = require('../middlewares/JWToken');
 
-const returnMsg = (errorMessage) => {
-  return {
-    error: true,
-    code: 'Bad Request',
-    message: errorMessage,
-  }
-};
+const returnMsg = (errorMessage) => ({ error: true, code: 'Bad Request', message: errorMessage });
 
 const create = async (body) => {
   const { displayName, email, password, image } = body;
   if (!/^[A-Za-z \s]{8,}$/.test(displayName) || !displayName) {
-    return returnMsg('"displayName" length must be at least 8 characters long')
-  };
-  if (!email) { return returnMsg('"email" is required')};
+    return returnMsg('"displayName" length must be at least 8 characters long');
+  }
+  if (!email) { return returnMsg('"email" is required'); }
   if (!/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/i.test(email)) {
-    return returnMsg('"email" must be a valid email')
-    };
-  if (!password) { returnMsg('"password" is required') };
-  if (password.length < 6) { returnMsg('"password" length must be 6 characters long') };
+    return returnMsg('"email" must be a valid email');
+    }
+  if (!password) { returnMsg('"password" is required'); }
+  if (password.length < 6) { returnMsg('"password" length must be 6 characters long'); }
   const emailExists = await Users.findOne({ where: { email } });
   if (emailExists) {
     return {
@@ -28,11 +22,8 @@ const create = async (body) => {
       message: 'Usuário já existe',
     };
   }
-  const newUser = await Users.create({ displayName, email, password, image });
-  const token = createToken({
-    displayName,
-    email,
-  });
+  await Users.create({ displayName, email, password, image });
+  const token = createToken({ displayName, email });
   return token;
 };
 
