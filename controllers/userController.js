3b74358a142e95513d98ae391b9591f4d19c9b92
Rafael Assad <rafaassadeng@gmail.com>
2021-01-27 19:48:01 -0300

    req 1 ajuda do avelar e andrezao

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 const service = require('../services/userServices');
 const createJWT = require('../token/createToken');
 const emailSword = require('../middlewares/emailSword');
-const user = require('../middlewares/user');
+const checkUser = require('../middlewares/checkUser');
 
 const route = Router();
 
-route.post('/', emailSword, user, async (req, res) => {
+route.post('/', emailSword, checkUser, async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
     const newUser = await service.create(displayName, email, password, image);
