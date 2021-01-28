3a085728168e878b0788daddaaeabba5a27ce640
pauloricardoz <trybe.przt@gmail.com>
2021-01-28 17:29:37 -0300

    req 3, 4 and 5 + lint

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const MiddleErrorUser = require('../middlewares/middlewareErrorUser');
+const MiddleToken = require('../middlewares/tokenMiddleware');
 const MiddleDisplayName = require('../middlewares/validDisplayName');
 const MiddleEmail = require('../middlewares/validEmail');
 const MiddlePassword = require('../middlewares/validPassword');
@@ -23,6 +24,25 @@ userRouter.post('/', middleWareUser, async (req, res, next) => {
     console.log(error);
     res.status(500).json({ message: 'Algo deu errado' });
   }
+});
+
+userRouter.get('/:id', MiddleToken, async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Users.findOne({ where: { id } });
+  if (!result) next({ status: 404, message: 'Usuário não existe' });
+  res.status(200).json(result);
+});
+
+userRouter.get('/', MiddleToken, async (req, res) => {
+  const result = await Users.findAll();
+  console.log(result);
+  res.status(200).json(result);
+});
+
+userRouter.delete('/me', MiddleToken, async (req, res) => {
+  const user = req.payload;
+  await Users.destroy({ where: { id: user.id } });
+  res.status(204).json();
 });
 
 userRouter.use(MiddleErrorUser);
--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,17 +1,21 @@
 const { verifyToken } = require('../functions/tokenOp');
 
 function MiddleToken(req, _res, next) {
-  const { Authorization } = req.headers;
+  const { authorization } = req.headers;
   const err = {
     message: 'Token não encontrado',
     status: 401,
   };
-  if (Authorization === undefined) {
+  if (authorization === undefined) {
+    return next(err);
+  }
+  err.message = 'Token não encontrado';
+  if (authorization === '') {
     return next(err);
   }
   err.message = 'Token expirado ou inválido';
   try {
-    const payload = verifyToken(Authorization);
+    const payload = verifyToken(authorization);
     req.payload = payload;
     next();
   } catch (error) {
