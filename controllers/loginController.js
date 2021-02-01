751f3a998a649853070fb3ddc6d5a50f847105e4
AndersonAGodoy <anderson.a.godoy@gmail.com>
2021-02-01 15:03:56 -0300

    requisito 2 feito

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@ const isLogged = require('../middleware/isLogged');
 
 const route = Router();
 
-route.post('/', isAuthenticate, isLogged, async (req, res) => {
+route.post('/', isLogged, isAuthenticate, async (req, res) => {
   const { token } = req;
 
   return res.status(200).json(token);
--- middleware/isLogged.js
+++ middleware/isLogged.js
@@ -1,11 +1,24 @@
 const isLogged = (req, res, next) => {
+  const errorMessage = '"password" is not allowed to be empty';
   const { email, password } = req.body;
   const regEx = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
+  if (email === '') {
+    return res
+      .status(400)
+      .json({ message: '"email" is not allowed to be empty' });
+  }
+  if (!email) {
+    return res.status(400).json({ message: '"email" is required' });
+  }
+  if (password === '') {
+    return res.status(400).json({ message: errorMessage });
+  }
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
   if (!email || !password || !regEx.test(email)) {
-    return res.status(400).json({
-      message: 'Campos inválidos',
-    });
+    return res.status(400).json({ message: 'Campos inválidos' });
   }
 
   next();
