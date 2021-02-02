14cdcaf68d20a4916dfbd83c5210bd1de006771d
AndersonAGodoy <anderson.a.godoy@gmail.com>
2021-02-02 13:57:22 -0300

    requisito 7 e 8 feitos

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,4 +13,18 @@ route.post('/', hasToken, validatePost, async (req, res) => {
   return res.status(201).json(newPost);
 });
 
+route.get('/', hasToken, async (_req, res) => {
+  const posts = await postService.getPosts();
+  return res.status(200).json(posts);
+});
+
+route.get('/:id', hasToken, async (req, res) => {
+  const { id } = req.params;
+  const post = await postService.getPostById(id);
+  if (!post) {
+    return res.status(404).json({ message: 'Post não existe' });
+  }
+  return res.status(200).json(post);
+});
+
 module.exports = route;
--- services/postService.js
+++ services/postService.js
@@ -1,10 +1,31 @@
-const { Posts } = require('../models');
+const { Posts, Users } = require('../models');
 
 const createPost = async (title, content, userId) => {
   const newPost = await Posts.create({ title, content, userId });
   return newPost;
 };
 
+const getPosts = () =>
+  Posts.findAll({
+    include: {
+      model: Users,
+      as: 'user',
+      attributes: ['id', 'displayName', 'email', 'image'],
+    },
+  });
+
+const getPostById = (id) =>
+  Posts.findOne({
+    where: { id },
+    include: {
+      model: Users,
+      as: 'user',
+      attributes: ['id', 'displayName', 'email', 'image'],
+    },
+  });
+
 module.exports = {
+  getPosts,
   createPost,
+  getPostById,
 };
