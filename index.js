ed2fbbf9da971a3fc95ffdf4720c7c548cbe8fe7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-15 20:38:09 -0300

    Inicia a rota User

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+require('./config/config');
+
 const app = express();
+
+const userRoutes = require('./routes/User.routes');
+
+app.use('/user', userRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
+// Ok Jean, não vou remover... bjs pra vc e fabiola...
 app.get('/', (request, response) => {
   response.send();
 });
