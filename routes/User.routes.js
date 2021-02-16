534e46f05c0e09229547085e7782d4e19f0e13d7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-16 19:23:35 -0300

    cria a rota de usuario

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const {
+  verifyDisplayName,
+  verifyEmail,
+  verifyPassword,
+} = require('../middlewares/UserMiddlwares');
+const UserController = require('../controllers/User.controller');
 
 const router = express.Router();
 
-router.post('/');
+router.post(
+  '/',
+  verifyDisplayName,
+  verifyEmail,
+  verifyPassword,
+  UserController.createUser,
+);
 
 module.exports = router;
