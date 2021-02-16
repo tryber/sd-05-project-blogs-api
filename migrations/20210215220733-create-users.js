53c08019d91441b2081a60235ff13c5a9524b02c
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-16 19:16:06 -0300

    Modifica umas coisas na migration de usuários

--- migrations/20210215220733-create-users.js
+++ migrations/20210215220733-create-users.js
@@ -5,7 +5,7 @@ module.exports = {
     const UserTable = queryInterface.createTable('Users', {
       id: {
         allowNull: false,
-        // autoIncrement: true,
+        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
