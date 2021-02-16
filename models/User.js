e9552a4fa181c0f3748c44392071f8fdc597e88d
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-16 19:17:11 -0300

    configura o timestamps para false

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
 const UserModel = (sequelize, DataTypes) => {
   const User = sequelize.define('Users', {
-    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  }, {
+    timestamps: false,
   });
 
   User.associate = (models) => {
