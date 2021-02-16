96a822c8bfc787bbeb96890b5fc328db5254c483
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-16 19:20:51 -0300

    Cria enum para os erros

---
+++ enumerators/ErrorsEnums.js
@@ -0,0 +1,8 @@
+module.exports = {
+  userExists: { message: 'Usuário já existe' },
+  invalidDisplayName: { message: '"displayName" length must be at least 8 characters long' },
+  invalidEmail: { message: '"email" must be a valid email' },
+  missingEmail: { message: '"email" is required' },
+  invalidPassword: { message: '"password" length must be 6 characters long' },
+  missingPassword: { message: '"password" is required' },
+};
