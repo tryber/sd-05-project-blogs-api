d554a356cedb2436c74678c0b25595179acdf474
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-02-18 13:33:10 -0300

    Cria AuthMiddlewares para trabalhar a autenticação

---
+++ middlewares/AuthMiddlewares.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const JWTConfigs = require('../config/JWTConfigs');
+const ErrorsEnum = require('../enumerators/ErrorsEnums');
+const { Users } = require('../models');
+
+const validateToken = async (token) => {
+  const payload = jwt.verify(token, JWTConfigs.SECRET);
+  const { userData: { dataValues } } = payload;
+  const user = await Users.findOne({ where: { email: dataValues.email } });
+  const { password, ...noPaswd } = user;
+  return noPaswd;
+};
+
+const tokenIsValid = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send(ErrorsEnum.missingToken);
+  }
+  try {
+    await validateToken(token);
+    next();
+  } catch (error) {
+    return res.status(401).json({ ...ErrorsEnum.invalidToken, error });
+  }
+};
+
+module.exports = { tokenIsValid };
