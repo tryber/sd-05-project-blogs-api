1f815736370d79b491f3fbe7582eb997798db263
carlanakajuni <carlanakajuni@hotmail.com>
2021-03-04 19:29:14 -0300

    Criando colunas e organizando a tbl Users

--- migrations/20210304221048-Users.js
+++ migrations/20210304221048-Users.js
@@ -2,6 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const Users = await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+    })
+    return Users;
     /**
      * Add altering commands here.
      *
@@ -10,12 +31,11 @@ module.exports = {
      */
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('Users')
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
 };
