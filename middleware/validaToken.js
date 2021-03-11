4ff5437e348cb69219e5b67e3d2fe37f8f60062a
ederusena <eder.sena@live.com>
2021-03-11 02:53:19 -0300

    chore: Delete user

--- middleware/validaToken.js
+++ middleware/validaToken.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 const validateToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+
     if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+
     const SECRET = process.env.SECRET || 'seicho-no-ie';
     const payload = jwt.verify(token, SECRET);
     req.userPayload = payload.userData;
     return next();
   } catch (err) {
-    console.log(err.message);
     return res.status(401).json({ message: 'Token expirado ou inválido' });
   }
 };
