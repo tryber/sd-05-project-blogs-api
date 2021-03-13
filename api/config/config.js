27d27f1dfdd2fb114bb0488d72b7252e4a1c989d
Preto115 <preto115@gmail.com>
2021-03-13 20:13:31 -0300

    alguem me ajuda com esse tal de: error  Calls to require() should use string literals  import/no-dynamic-require

--- api/config/config.js
+++ api/config/config.js
@@ -1,17 +1,18 @@
-require('dotenv').config();
+require('dotenv/config');
 
+// console.log(process.env.API_NAME)
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    username: process.env.DB_MYSQL_USER,
+    password: process.env.DB_MYSQL_PASSWORD,
+    database: process.env.API_NAME,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: process.env.API_NAME,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000;
 routes(app);
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => { response.send("Chegou!")});
+app.get('/', (_request, response) => {
+  response.send();
+});
 
 app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}!`));
 // module.exports = app;
