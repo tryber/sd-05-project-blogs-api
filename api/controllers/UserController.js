d663337816a71ba74f2d183da5b1e4efc9ab4d8c
Preto115 <preto115@gmail.com>
2021-03-14 20:54:36 -0300

    implentando controller de usuario ppara fazer login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,4 +1,5 @@
 const database = require('../models');
+const generateToken = require('../services');
 
 class UserController {
   static async cadastraUsuario(req, res) {
@@ -9,7 +10,29 @@ class UserController {
     } catch (error) {
       if (error.message === 'Validation error') {
         return res.status(409).json({ message: 'Usuário já existe' });
-      } res.status(500).json(error.message);
+      }
+      res.status(500).json(error.message);
+    }
+  }
+
+  static async efetuaLogin(req, res) {
+    const { email, password } = req.body;
+    console.log('email procurado: ', email);
+    try {
+      const usuarioLogado = await database.Users.findOne({
+        where: {
+          email,
+        },
+      });
+      console.log('usuario encontrado: ', usuarioLogado);
+      if (usuarioLogado && password === usuarioLogado.password) {
+        console.log('senha: ', password, 'senhaencontrada: ', usuarioLogado.password);
+        const token = await generateToken(usuarioLogado);
+        return res.status(200).json({ token });
+      }
+      return res.status(400).json({ message: 'Campos inválidos' });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
   }
 }
