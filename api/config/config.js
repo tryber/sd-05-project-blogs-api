6358a2f72d5e33cc73877954a8122e945e66a330
Preto115 <preto115@gmail.com>
2021-03-14 02:40:18 -0300

    modificando arquivos de configuração

--- api/config/config.js
+++ api/config/config.js
@@ -1,26 +1,27 @@
-require('dotenv/config');
-
-// console.log(process.env.API_NAME)
+// require('dotenv').config({
+//   path: (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development")
+//   ? ".env" : ".env"
+// })
 module.exports = {
   development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.DB_MYSQL_PASSWORD,
+    database: process.env.API_NAME,
+    host: process.env.HOSTNAME,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
     username: process.env.DB_MYSQL_USER,
     password: process.env.DB_MYSQL_PASSWORD,
     database: process.env.API_NAME,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.API_NAME,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: process.env.API_NAME,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
   },
 };
