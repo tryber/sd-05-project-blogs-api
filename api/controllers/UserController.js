60f4f1500773fdeec5350cd07f1b46126ac7f137
Preto115 <preto115@gmail.com>
2021-03-14 22:28:30 -0300

    implementando controller  para buscar buscar um usuario no banco e deletar

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -45,6 +45,39 @@ class UserController {
     }
   }
 
+  static async buscaUm(req, res) {
+    try {
+      const { id } = req.params;
+      const pessoa = await database.Users.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      if (pessoa !== null) {
+        return res.status(200).json(pessoa);
+      }
+      return res.status(404).send('Usuário não existe');
+    } catch (error) {
+      res.status(500).json({ erro: error.message });
+    }
+  }
+
+  static async deletaProprioUsuario(req, res) {
+    try {
+      const { me: { email } } = req;
+      const pessoaDeletada = await database.Users.destroy({
+        where: {
+          email,
+        },
+      });
+
+      if (!pessoaDeletada) return res.status(404).send('Esta pessoa nao existe no banco de dados.');
+      res.status(204).send('Pessoa deletada com sucesso!');
+    } catch (error) {
+      return res.status(500).json({ erro: error.message });
+    }
+  }
+
   static async buscaPorEmail(email) {
     try {
       const usuarioLogado = await database.Users.findOne({
@@ -54,7 +87,7 @@ class UserController {
       });
       return usuarioLogado;
     } catch (error) {
-      return ({ message: error.message });
+      return { message: error.message };
     }
   }
 }
