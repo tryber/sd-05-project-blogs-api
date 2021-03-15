6241c41fac78cf445a5b318b49a559c43df53b55
Preto115 <preto115@gmail.com>
2021-03-14 22:07:50 -0300

    implentando a rota para buscar todos os usuarios

--- api/routes/usersRouter.js
+++ api/routes/usersRouter.js
@@ -1,11 +1,12 @@
 const rescue = require('express-rescue');
 const { Router } = require('express');
-const { cadastro, login } = require('../middlewares');
+const { cadastro, login, tokenValidation } = require('../middlewares');
 const UserController = require('../controllers/UserController');
 
 const route = Router();
 
 route.post('/user', cadastro, rescue(UserController.cadastraUsuario));
 route.post('/login', login, rescue(UserController.efetuaLogin));
+route.get('/user', tokenValidation, rescue(UserController.buscaTodosUsuarios));
 
 module.exports = route;
