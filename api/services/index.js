6fe44b1c60156b2ba9428e89fcfef481d8ce74c9
Preto115 <preto115@gmail.com>
2021-03-15 19:27:39 -0300

    separando funcionalidades e separando na pasta services

--- api/services/index.js
+++ api/services/index.js
@@ -1,18 +1,7 @@
-const jwt = require('jsonwebtoken');
-const secretkey = require('../models/secretKey');
+const attributesExtrator = require('./postAttributesExtrator');
+const tokenGenerator = require('./tokenGenerator');
 
-const secret = secretkey();
-
-module.exports = async (user) => {
-  const jwtConfig = {
-    expiresIn: '30min',
-    algorithm: 'HS256',
-  };
-
-  const { password, ...withoutPassword } = user;
-  const usuario = withoutPassword;
-  console.log('sem password:', usuario);
-
-  const token = jwt.sign({ data: usuario }, secret, jwtConfig);
-  return token;
+module.exports = {
+  attributesExtrator,
+  tokenGenerator,
 };
