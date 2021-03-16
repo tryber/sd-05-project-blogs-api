cbc66b6f5825b883ba62fbbeba7cfe4db7204038
Preto115 <preto115@gmail.com>
2021-03-16 09:34:25 -0300

    revisando o arquivo index da raiz

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const rescue = require('express-rescue');
 const express = require('express');
-const routes = require('./api/routes');
 const { tokenValidation } = require('./api/validations');
 const { buscaPostPorTermo } = require('./api/controllers/PostController');
+const routes = require('./api/routes');
 
 require('dotenv').config();
 
-console.log(`testando ambiente: ${process.env}`);
+console.log(`testando ambiente: ${process.env.NODE_ENV}`);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-routes(app);
 app.get('/post/search', tokenValidation, rescue(buscaPostPorTermo));
-
+app.get('/', (_request, response) => { response.send(); });
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
+
+routes(app);
+app.use((err, _req, res, _next) => {
+  if (err.status) {
+    console.log('deu ruim!!!');
+    return res.status(err.status).json(err);
+  }
 });
 
 app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}!`));
