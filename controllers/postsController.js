b18b6e98a8a66507a3688c297bc939fe04bf9079
rafaelpbar <rafael.coper@outlook.com>
2021-03-15 22:56:51 -0300

    Não esta passando nos testes

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const { Op } = require('sequelize');
+
 const router = express.Router();
 
 const { Posts, Users } = require('../models');
 const { verifyJWT } = require('../middleware/authorization');
 const { validateTitle, validateContent } = require('../middleware/postValidation');
-// const { Op } = require('sequelize');
 
 router.post('/',
   verifyJWT,
@@ -14,11 +15,11 @@ router.post('/',
   async (req, res) => {
     try {
       const { title, content } = req.body;
+      console.log('postController L 18', req.body);
       const { id } = req.payload;
-      console.log('postController L 17', req.body);
-      console.log('postController L 18', req.payload);
+      console.log('postController L 20', req.payload);
       const post = await Posts.create({ title, content, userId: id });
-      console.log('postController L19', post);
+      console.log('postController L22', post);
       return res.status(201).json(post);
     } catch (err) {
       if (err) return res.status(400).json({ message: 'Algo deu errado' });
@@ -66,21 +67,27 @@ router.put('/:id',
     return res.status(200).json(postUpdated);
   });
 
-// router.get('/search',
-//   verifyJWT,
-//   async (req, res) => {
-//     const { q } = req.query;
-//     const post = await Posts.findAll(
-//       { where: },
-//       {
-//         include: [{
-//           model: Users,
-//           as: 'user',
-//           attributes: { exclude: ['userId'] }
-//         }]
-//     });
-//     if (post === null) return res.status(404).json({ message: 'Post not found' });
-//     return res.status(200).json(post);
-//   });
+router.get('/search',
+  verifyJWT,
+  async (req, res) => {
+    try {
+      const { q } = req.query;
+      console.log('aqui no q', q);
+      const post = await Posts.findAll({
+        where: {
+          [Op.or]: [{ title: { [Op.like]: `%${q}%` } },
+            { content: { [Op.like]: `%${q}%` } },
+          ],
+        },
+        include: [{ model: Users, as: 'user' }],
+        attributes: { exclude: ['userId'] },
+      });
+      console.log('post aqui na L88', post);
+      if (!post) return res.status(404).json({ message: 'Post not found' });
+      return res.status(200).json(post);
+    } catch (err) {
+      return res.status(404).json({ message: 'caiu no catch' });
+    }
+  });
 
 module.exports = router;
