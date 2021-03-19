b5a8752df5d9080b18ed1fd241d494f3e93d2db5
saturnian99 <saturnian.dsg@gmail.com>
2021-03-19 17:08:51 -0300

    PORT env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send();
 });
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
