7b751975c5ecb6cfe6804aad54a2535b0528e8ee
Larissa Palombo <larissampalombo@gmail.com>
2021-03-19 15:45:53 -0300

    user controller

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+// requisição do frontend
+const express = require('express');
+const { modelUser } = require('../models');
+const routeUser = express.Router();
+
+routeUser.post('/', (req, res) => {
+  const { displayName, email, password, image } = req.body;
+  modelUser
+    .create({ displayName, email, password, image })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({ message: 'Algo deu errado' });
+    });
+});
+
+module.exports = routeUser;
