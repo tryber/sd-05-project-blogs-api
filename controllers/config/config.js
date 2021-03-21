aeba8982e49708615d4779d022de323fde3dd1e2
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-03-21 17:41:30 -0300

    primeiro commit

---
+++ controllers/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '/home/hugo/Documentos/trybe/projetos_T5/projetos-T5-recuperacao/blogs-api/sd-05-project-blogs-api/.env' });
+
+console.log(`${__dirname}`);
+
+console.log(process.env.MYSQL_USER);
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
---
+++ controllers/createUser.js
@@ -0,0 +1,7 @@
+// const connection = require('./connection');
+
+// const createUser = async (name, email, password) =>
+//   connection()
+//     .then((db) => db.collection('users').insertOne({ name, email, password }));
+
+// module.exports = createUser;
