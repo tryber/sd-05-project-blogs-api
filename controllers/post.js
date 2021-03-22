6c46adc3d8cb1f40c941667a477e870d847695e6
Sidnei Ramos <sidneiramos02@gmail.com>
2021-03-22 16:47:57 -0300

    Get posts

--- controllers/post.js
+++ controllers/post.js
@@ -2,23 +2,30 @@ const { Router } = require('express');
 const { verifyToken } = require('../midllewares/jwt');
 const { verifyTitle, verifyContent } = require('../midllewares/validation');
 // const { createToken } = require('../midllewares/jwt');
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 
 const postRouter = Router();
 
 postRouter.post('/', verifyToken, async (req, res) => {
   try {
     const { title, content } = req.body;
-    console.log(req.body, 'requisição');
     const { id } = req.payload;
     verifyTitle(title);
     verifyContent(content);
     const post = await Post.create({ title, content, userId: id });
-    console.log(post, 'post');
     return res.status(201).json(post);
   } catch (err) {
-    console.log(err, 'erro');
     return res.status(err.status).json({ message: err.message });
+  }
+});
+
+postRouter.get('/', verifyToken, async (_req, res) => {
+  try {
+    const posts = await Post.findAll({ include: { model: User, as: 'user' } });
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.log(err, 'erro');
+    return res.status(500).json({ message: 'Deu ruim' });
   }
 });
 
