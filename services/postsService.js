cd0832f6715aed45ac8741a53478af389f56ceb3
Ishidor <kevinmendessilva@hotmail.com>
2021-03-22 16:26:39 -0300

    teste

---
+++ services/postsService.js
@@ -0,0 +1,69 @@
+const { Op } = require('sequelize');
+const { Post, User } = require('../models');
+const throwErr = require('../utils/throwErr');
+
+const createPost = async (title, content, userId) => {
+  if (!title) return throwErr('bad-request', '"title" is required');
+
+  if (!content) return throwErr('bad-request', '"content" is required');
+
+  return Post.create({ title, content, userId });
+};
+
+const getAllPosts = async () => Post.findAll({ include: { model: User, as: 'user', attributes: { exclude: ['password'] } } });
+
+const getOnePost = async (id) => {
+  const post = await Post.findOne({ where: { id }, include: { model: User, as: 'user', attributes: { exclude: ['password'] } } });
+
+  if (!post) return throwErr('not-found', 'Post não existe', 404);
+
+  return post;
+};
+
+const editPost = async (id, userId, title, content) => {
+  if (!title) return throwErr('bad-request', '"title" is required');
+
+  if (!content) return throwErr('bad-request', '"content" is required');
+
+  if (id !== userId) return throwErr('unauthorized', 'Usuário não autorizado', 401);
+
+  const post = await Post.update({ title, content }, { where: { id, userId } });
+
+  if (!post) return throwErr('not-found', 'Post não existe', 404);
+
+  return post;
+};
+
+const deletePost = async (id, userId) => {
+  const postExists = await Post.findOne({ where: id, userId });
+
+  if (!postExists) return throwErr('not-found', 'Post não existe', 404);
+
+  if (id !== userId) return throwErr('unauthorized', 'Usuário não autorizado', 401);
+
+  return Post.destroy({ where: { id, userId } });
+};
+
+const getPostsBySearch = async (query) => {
+  const posts = await Post.findAll(
+    { where: {
+      [Op.or]: [
+        { title: { [Op.like]: `%${query}%` } },
+        { content: { [Op.like]: `%${query}%` } },
+      ],
+    },
+    include: {
+      model: User, as: 'user', attributes: { exclude: ['password'] } } },
+  );
+
+  return posts;
+};
+
+module.exports = {
+  createPost,
+  getAllPosts,
+  getOnePost,
+  editPost,
+  deletePost,
+  getPostsBySearch,
+};
---
+++ services/usersService.js
@@ -0,0 +1,59 @@
+const { User } = require('../models');
+const validateDisplayName = require('../utils/validateDisplayName');
+const validateEmail = require('../utils/validateEmail');
+const validatePassword = require('../utils/validatePassword');
+const throwErr = require('../utils/throwErr');
+
+const createUser = async (displayName, email, password, image) => {
+  if (!validateDisplayName(displayName)) return throwErr('bad-request-name', '"displayName" length must be at least 8 characters long');
+
+  if (!email) return throwErr('bad-request', '"email" is required');
+
+  if (!validateEmail(email)) return throwErr('bad-request', '"email" must be a valid email');
+
+  if (!password) return throwErr('bad-request', '"password" is required');
+
+  if (!validatePassword(password)) return throwErr('bad-request', '"password" length must be 6 characters long');
+
+  const userAlreadyExists = await User.findOne({ where: { email } });
+
+  if (userAlreadyExists) return throwErr('conflict', 'Usuário já existe', 409);
+
+  return User.create({ displayName, email, password, image });
+};
+
+const login = async (email, password) => {
+  if (email === '') return throwErr('bad-request', '"email" is not allowed to be empty');
+
+  if (password === '') return throwErr('bad-request', '"password" is not allowed to be empty');
+
+  if (!email) return throwErr('bad-request', '"email" is required');
+
+  if (!password) return throwErr('bad-request', '"password" is required');
+
+  const user = await User.findOne({ where: { email } });
+
+  if (!user) return throwErr('bad-request', 'Campos inválidos');
+
+  return user;
+};
+
+const getAllUsers = async () => User.findAll();
+
+const getOneUser = async (id) => {
+  const user = await User.findOne({ where: { id } });
+
+  if (!user) return throwErr('not-found', 'Usuário não existe', 404);
+
+  return user;
+};
+
+const deleteActualUser = async (id) => User.destroy({ where: { id } });
+
+module.exports = {
+  createUser,
+  login,
+  getAllUsers,
+  getOneUser,
+  deleteActualUser,
+};
