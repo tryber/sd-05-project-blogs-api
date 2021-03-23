722636578d6965b6d81403cd1aa9ac4909efa7ca
Alexander Mitchell <alexlm.trybe@gmail.com>
2021-03-23 02:18:56 -0300

    Odeio findByPk e irei destrui-lo

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,9 +20,33 @@ postsRouter.post('/', tokenValidation, errorHandler(schema), async (req, res) =>
   const { title, content } = req.body;
   const { id } = req.payload;
   try {
-
     const newPost = await Posts.create({ title, content, userId: id });
-    return res.status(201).json(newPost);
+    return res.status(201).json(newPost.dataValues);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: err });
+  }
+});
+
+postsRouter.get('/', tokenValidation, async (req, res) => {
+  try {
+    const posts = await Posts.findAll({ include: { model: Users, as: 'user' } });
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: err });
+  }
+});
+
+postsRouter.get('/:id', tokenValidation, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const posts = await Posts.findByPk(id, { include: { model: Users, as: 'user' } });
+    console.log(posts, 'SSIIIIIIDDDDD');
+    if (!posts) {
+      return res.status(404).json({ message: 'Post não existe' });
+    }
+    return res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     return res.status(400).json({ message: err });
