19d2794e078e6d2d77de1f214beb8db5cd4277ff
saturnian99 <saturnian.dsg@gmail.com>
2021-03-23 09:09:16 -0300

    Fixinn error middleware; Added bodyparser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { userController } = require('./controllers');
 require('dotenv').config();
 
 const app = express();
@@ -10,11 +12,16 @@ app.get('/', (_req, res) => {
   res.send();
 });
 
+app.use('/', bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/user', userController);
+
 const errorMiddleware = (err, _req, res, _next) => {
-  if (err.status) res.status(err.status).json({ message: err.message });
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  console.error(err);
   res.status(500).json({ message: 'Something went wrong :(' });
 };
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+app.listen(PORT, () => console.log(`Online on ${PORT}!`));
